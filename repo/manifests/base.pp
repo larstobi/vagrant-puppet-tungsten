a6600f844ce35cd96e4b69b3b6aa6f194bf0ad73
Roman Heinrich <roman.heinrich@gmail.com>
2012-11-20 20:30:00 +0200

    stage definition

--- repo/manifests/base.pp
+++ repo/manifests/base.pp
@@ -1,4 +1,10 @@
 File { owner => 0, group => 0, mode => 0644 }
+Exec { path => "/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin/:/bin:/sbin" }
+
+stage { 'first':}
+stage { 'last': }
+Stage['first'] -> Stage['main'] -> Stage['last']
 
 import 'roles/*.pp'
 import 'nodes.pp'
+
